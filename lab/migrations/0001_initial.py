bf5c81c4649998ce2a2ee99e347aa644b6d47b97
Daniel <danor.dcor@gmail.com>
2017-11-29 16:44:51 -0500

    migraciones

---
+++ lab/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Estudiante',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('Nombre', models.CharField(max_length=45)),
+                ('Apellido', models.CharField(max_length=45)),
+            ],
+        ),
+    ]
--- lab/models.py
+++ lab/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+SEX = (('M', "Masculino"),
+       ('F', "Femenino"),)
+
+class Estudiante(models.Model):
+    Nombre = models.CharField(max_length=45)
+    Apellido = models.CharField(max_length=45)
