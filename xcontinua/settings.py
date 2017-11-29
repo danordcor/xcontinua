14d5c3a1504fd93ced08c79ecbcace924e9b4f7f
Daniel <danor.dcor@gmail.com>
2017-11-29 16:34:38 -0500

    app

--- xcontinua/settings.py
+++ xcontinua/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
+    'material.theme.lightblue',
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,11 +78,10 @@ WSGI_APPLICATION = 'xcontinua.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
