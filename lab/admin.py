e9060a6dacc2896cd795142a0bbe9b259e4d719b
Daniel <danor.dcor@gmail.com>
2017-11-29 16:53:32 -0500

    Admin

--- lab/admin.py
+++ lab/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Estudiante
 
 # Register your models here.
 admin.site.site_header = 'Integración Continua, Entrega Continua y Despliegue Continuo'
+admin.site.register(Estudiante)
