a96ce4bbc323f501eff215e09c0e3e1998913169
Daniel <danor.dcor@gmail.com>
2017-11-29 16:58:43 -0500

    Admin

--- lab/models.py
+++ lab/models.py
@@ -4,6 +4,10 @@ from django.db import models
 SEX = (('M', "Masculino"),
        ('F', "Femenino"),)
 
+
 class Estudiante(models.Model):
     Nombre = models.CharField(max_length=45)
     Apellido = models.CharField(max_length=45)
+
+    def __str__(self):
+        return str(self.Nombre + self.Apellido)
